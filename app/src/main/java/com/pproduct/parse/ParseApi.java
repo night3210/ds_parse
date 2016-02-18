1a7a77e7dbcd2aaa5d1c12f3e0066140f30e8064
Ivan Guy <ivan.guy@p-product.com>
2016-02-18 16:08:17 +0200

    add jitpack dependency

---
+++ app/src/main/java/com/pproduct/parse/ParseApi.java
@@ -0,0 +1,7 @@
+package com.pproduct.parse;
+
+/**
+ * Created by Developer on 2/12/2016.
+ */
+public class ParseApi {
+}
---
+++ app/src/main/java/com/pproduct/parse/ParseOnlineQueryFetch.java
@@ -0,0 +1,52 @@
+package com.pproduct.parse;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
+import com.pproduct.datasource.core.ListDataSource;
+import com.pproduct.datasource.core.fetch_result.BaseFetchResult;
+import com.pproduct.datasource.core.listeners.DataCallback;
+import com.pproduct.datasource.core.listeners.Fetch;
+
+/**
+ * Created by Developer on 2/12/2016.
+ */
+public class ParseOnlineQueryFetch implements Fetch {
+    ParseQuery query = null;
+    public ParseOnlineQueryFetch(ParseQuery query) {
+        this.query = query;
+    }
+    public void setQuery(ParseQuery query) {
+        this.query = query;
+    }
+    @Override
+    public void fetchOnline(ListDataSource.Paging paging, final DataCallback callback){
+        if(paging!=null) {
+            query.setSkip(paging.getSkip());
+            query.setLimit(paging.getLimit());
+        }
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List objects, ParseException e) {
+                if(e==null)
+                    callback.onSuccess(objects);
+                else
+                    callback.onError(e);
+            }
+        });
+    }
+
+    @Override
+    public void fetchOffline(DataCallback callback) {
+
+    }
+
+    @Override
+    public void storeItems(BaseFetchResult result) {
+
+    }
+}
